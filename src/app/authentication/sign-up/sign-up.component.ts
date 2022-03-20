54900aa9fa63595f77a371f920b51f16145fe656
AlexandrBelous <alexandrbelous2003@gmail.com>
2022-03-20 10:23:31 +0500

    sign-up

--- src/app/authentication/sign-up/sign-up.component.ts
+++ src/app/authentication/sign-up/sign-up.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent {
 
-  constructor() { }
+  hide = true;
+  public signUpForm: FormGroup;
 
-  ngOnInit(): void {
+
+  constructor(
+
+  ) { 
+    this.signUpForm = new FormGroup({
+      name: new FormControl(''),
+      login: new FormControl(''),
+      password: new FormControl(''),
+    });
   }
-
+  onSubmit(){
+    console.log(this.signUpForm)
+  }
 }
